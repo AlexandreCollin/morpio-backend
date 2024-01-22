278267acfd53aa936587f0c6ca80fa5928c589f7
Alexandre Collin <alexandre.collin-betheuil@epitech.eu>
2024-01-22 23:59:57 +0100

    feat: add win condituin

--- src/morpio/morpio.service.ts
+++ src/morpio/morpio.service.ts
@@ -41,6 +41,8 @@ class MorpioGame {
   private playerToPlay: number = 1;
   private readonly gridSize: number;
   private grid: CellValue[][];
+  private turn: number = 1;
+  private readonly winCondition: number = 3;
 
   constructor(creator: Socket, maxPlayers: number = 2, gridSize: number = 3) {
     this.maxPlayers = maxPlayers;
@@ -69,6 +71,17 @@ class MorpioGame {
 
     if (event == 'play') {
       const { row, column } = data;
+
+      if (this.grid[row][column] != CellValue.Empty) {
+        player.send({
+          event: 'illegalPlay',
+          data: { row: row, column: column },
+        });
+        return;
+      }
+
+      this.grid[row][column] = player.cellValue;
+
       this.players.forEach((elem) => {
         if (elem.id == this.playerToPlay) return;
         elem.send({
@@ -76,9 +89,126 @@ class MorpioGame {
           data: { row: row, column: column, cellValue: player.cellValue },
         });
       });
+
+      if (this.turn >= this.winCondition) {
+        if (this.checkWin(row, column)) {
+          this.players.forEach((elem) => {
+            elem.send({ event: 'gameOver', data: player.cellValue });
+          });
+        }
+      }
+
+      if (this.turn == (this.gridSize * this.gridSize) / 2 + 0.5) {
+        this.players.forEach((elem) => {
+          elem.send({ event: 'gameOver', data: CellValue.Empty });
+        });
+      }
+
+      this.turn += 0.5;
       this.playerToPlay = this.playerToPlay == 1 ? 2 : 1;
     }
   };
+
+  private checkLine(row: number, column: number) {
+    const cellValue = this.grid[row][column];
+    let left: number = column - this.winCondition + 1;
+    let right: number = column + this.winCondition - 1;
+    let count = 0;
+
+    if (left < 0) {
+      left = 0;
+    }
+    if (right > this.gridSize) {
+      right = this.gridSize;
+    }
+
+    for (let i = left; i < right; i++) {
+      if (this.grid[row][i] == cellValue) {
+        count++;
+      } else {
+        count = 0;
+      }
+    }
+
+    return count == this.winCondition;
+  }
+
+  private checkColumn(row: number, column: number) {
+    const cellValue = this.grid[row][column];
+    let top: number = row - this.winCondition + 1;
+    let bottom: number = row + this.winCondition - 1;
+    let count = 0;
+
+    if (top < 0) {
+      top = 0;
+    }
+    if (bottom > this.gridSize) {
+      bottom = this.gridSize;
+    }
+
+    for (let i = top; i < bottom; i++) {
+      if (this.grid[i][column] == cellValue) {
+        count++;
+      } else {
+        count = 0;
+      }
+    }
+
+    return count == this.winCondition;
+  }
+
+  private checkDiagonal(row: number, column: number) {
+    const cellValue = this.grid[row][column];
+    let top: number = row - this.winCondition + 1;
+    let bottom: number = row + this.winCondition - 1;
+    let left: number = column - this.winCondition + 1;
+    let right: number = column + this.winCondition - 1;
+    let countLeftToRight: number = 0;
+    let countRightToLeft: number = 0;
+
+    if (top < 0) {
+      top = 0;
+    }
+    if (bottom > this.gridSize) {
+      bottom = this.gridSize;
+    }
+    if (left < 0) {
+      left = 0;
+    }
+    if (right > this.gridSize) {
+      right = this.gridSize;
+    }
+
+    let i = top;
+    let j = left;
+    while (i < bottom && j < right) {
+      if (this.grid[i][j] == cellValue) {
+        countLeftToRight++;
+      } else {
+        countLeftToRight = 0;
+      }
+      if (this.grid[i][right - j] == cellValue) {
+        countRightToLeft++;
+      } else {
+        countRightToLeft = 0;
+      }
+      i++;
+      j++;
+    }
+
+    return (
+      countLeftToRight == this.winCondition ||
+      countRightToLeft == this.winCondition
+    );
+  }
+
+  private checkWin(row: number, column: number): boolean {
+    return (
+      this.checkLine(row, column) ||
+      this.checkColumn(row, column) ||
+      this.checkDiagonal(row, column)
+    );
+  }
 
   private startGame() {
     this.players.forEach((player, index) => {
